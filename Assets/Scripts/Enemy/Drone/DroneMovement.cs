9c9dd728862bdf7df113d8601ac8ecd871f274d8
EdvinFlodstrom <118813992+EdvinFlodstrom@users.noreply.github.com>
2023-03-17 11:56:38 +0100

    2023-03-07 11:55 Drone object and sprites.

---
+++ Assets/Scripts/Enemy/Drone/DroneMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    [SerializeField] private Transform firepoint;
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [SerializeField] private float speed;
+    private bool movingLeft;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemy/Drone/DroneProjectile.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DroneProjectile : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
